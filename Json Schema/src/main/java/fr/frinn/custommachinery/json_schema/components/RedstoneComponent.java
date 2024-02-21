7c4b06469d16737fb72c0086404632c795778d6b
alec016 <78179790+alec016@users.noreply.github.com>
2024-02-21 19:50:38 +0100

    fix: redstoneComponent.comparatorinputtype type

--- Json Schema/src/main/java/fr/frinn/custommachinery/json_schema/components/RedstoneComponent.java
+++ Json Schema/src/main/java/fr/frinn/custommachinery/json_schema/components/RedstoneComponent.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 
 public class RedstoneComponent extends Component {
   @JsonProperty(access = JsonProperty.Access.READ_WRITE)
@@ -24,19 +25,7 @@ public class RedstoneComponent extends Component {
   @Max(16)
   private int erroredpoweroutput;
   @JsonProperty(access = JsonProperty.Access.READ_WRITE)
-  @JsonSubTypes({
-    @JsonSubTypes.Type(value = ChunkLoadComponent.class, name = "custommachinery:chunkload"),
-    @JsonSubTypes.Type(value = EnergyComponent.class, name = "custommachinery:energy"),
-    @JsonSubTypes.Type(value = ExperienceComponent.class, name = "custommachinery:experience"),
-    @JsonSubTypes.Type(value = FluidComponent.class, name = "custommachinery:fluid"),
-    @JsonSubTypes.Type(value = ItemComponent.class, name = "custommachinery:item"),
-    @JsonSubTypes.Type(value = RedstoneComponent.class, name = "custommachinery:redstone")
-  })
-  @JsonTypeInfo(
-    use = JsonTypeInfo.Id.NAME,
-    include = JsonTypeInfo.As.EXISTING_PROPERTY,
-    property = "type"
-  )
+  @Pattern(regexp = "custommachinery:[energy|fluid|item]")
   private String compratorinputtype;
   @JsonProperty(access = JsonProperty.Access.READ_WRITE)
   private String comparatorinputid;
