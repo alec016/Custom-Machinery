bd880e3090ed7ce675ae0dd8c111a6cf09765988
alec016 <78179790+alec016@users.noreply.github.com>
2024-02-24 13:16:35 +0100

    fix: int range pattern

--- Json Schema/src/main/java/fr/frinn/custommachinery/json_schema/recipe/requirements/LightRequirement.java
+++ Json Schema/src/main/java/fr/frinn/custommachinery/json_schema/recipe/requirements/LightRequirement.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Pattern;
 
 public class LightRequirement extends Requirement {
-  public static final String INT_RANGE = "[[\\[|\\(]?]\\d+,?\\d[\\]|\\)]+]|\\d|\\*|]";
+  public static final String INT_RANGE = "[[\\[|\\(]]\\d+,?\\d+[\\]|\\)]]+|\\d|\\*|]";
   @JsonProperty(access = JsonProperty.Access.READ_WRITE, required = true)
   @Pattern(regexp = INT_RANGE)
   private String light;
